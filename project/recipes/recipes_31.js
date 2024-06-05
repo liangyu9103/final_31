54a82c596dcce4e639c3716bf9ed96c735b680c9
liangyu9103 <liangyu9103@gmail.com>
2024-06-05 23:31:32 +0800

    更改顯示樣式

--- project/recipes/recipes_31.js
+++ project/recipes/recipes_31.js
@@ -2,6 +2,7 @@
 import { _supabase } from './recipesSupabase_31.js';
 
 const productContainer = document.querySelector('.products-container');
+const recipesContainer = document.querySelector('#recipes-detail');
 
 // console.log('products_31', products_31);
 
@@ -19,28 +20,95 @@ const getRecipesSupabase_31 = async () => {
   }
 };
 
+function showModal(id) {
+  console.log(id);
+  console.log('test');
+  if (!id) return;
+  document.getElementById(id).style.display = 'unset';
+}
+
+function closeModal(id) {
+  if (!id) return;
+  document.getElementById(id).style.display = 'none';
+}
+
 const displayProducts = (products) => {
-  let productsContent = products
+  // 料理簡介
+  let introContent = products
     .map((product) => {
-      const { title, description, localimg } = product;
+      const { title, description, localimg, id } = product;
       //const { id } = product;
       return `
     <div class="single-product">
-    <img
-      src=${localimg}
-      class="single-product-img img"
-      alt=${title}
-    />
-    <footer class="card-footer">
-      <h3 class="name">${title}</h3>
-      <span class="price">${description}</span>
-      <div class="recipes-detail"><button class="recipes-detail-btn">詳細資訊</button></div>
-    </footer>
-  </div>
-    `;
+      <img
+        src=${localimg}
+        class="single-product-img img"
+        alt=${title}
+      />
+      <footer class="card-footer">
+        <h3 class="name">${title}</h3>
+        <span class="price">${description}</span>
+        <a href="#">
+          <div class="recipes-detail"><button id="${id}-btn" class="recipes-detail-btn">詳細資訊</button></div>
+        </a>
+      </footer>
+    </div>
+      `;
     })
     .join('');
-  productContainer.innerHTML = productsContent;
+
+    let recipesContent = products
+      .map((product) => {
+        const { id, title, localimg, ingredients, step1, step2, step3, step4, step5 } = product;
+        return `
+        <div id=${id} class="modal hidden">
+          <div class="recipes-detail-card">
+            <div class="recipes-detail-card-left">
+              <h2>${title}</h2>
+              <img
+                src=${localimg}
+                alt=${title}
+              />
+            </div>
+            <div class="recipes-detail-card-right">
+              <h2 class="mb-unset">食材：</h2>
+              <h4>
+                ${ingredients}
+              </h4>
+              <h2 class="mb-unset">步驟：</h>
+              <h4 class="mb-unset">
+                ${step1}
+              </h4>
+              <h4 class="mb-unset">
+                ${step2}
+              </h4>
+              <h4 class="mb-unset">
+                ${step3}
+              </h4>
+              <h4 class="mb-unset">
+                ${step4}
+              </h4>
+              <h4 class="mb-unset">
+                ${step5}
+              </h4>
+              <div style="margin: 0.5rem; text-align: right">
+                <button id="${id}-close" class=close-modal-btn>關閉</button>
+              </div>
+            </div>
+          </div>
+        </div>
+        `;
+      })
+      .join('');
+  productContainer.innerHTML = introContent;
+  recipesContainer.innerHTML = recipesContent;
+  setTimeout(() => {
+    products.map((item) => {
+      const { id } = item;
+      document.getElementById(id + '-btn').addEventListener('click', function () { showModal(id) });
+      document.getElementById(id + '-close').addEventListener('click', function () { closeModal(id) });
+    })
+  }, 500)
 };
 
 // const DisplayProducts = (products) => {
@@ -85,8 +153,6 @@ const displayProducts = (products) => {
 document.addEventListener('DOMContentLoaded', async () => {
   products2_31 = await getRecipesSupabase_31();
   displayProducts(products2_31);
-  // const introTab = document.getElementById('recipes-intro');
-  // introTab.addEventListener('click', clickTab('recipes-intro'));
   // const detailTab = document.getElementById('recipes-detail');
   // detailTab.addEventListener('click', clickTab('recipes-detail'));
 });
